6f95c65e0d9ea1188da8867e231e40ef84fd7d46
Aayushman Jha <aayushman.jha.dev@gmail.com>
2026-06-21 19:03:14 +0530

    Add Order Summary

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,5 +34,42 @@ with col6:
     st.image("https://images.pexels.com/photos/15476368/pexels-photo-15476368.jpeg",width=200)
     VegBurg_count = st.number_input("Veggie Veg Burger@ 90",min_value=0,max_value=10,step=1,key="vegburger")
 
+menu_items={
+    "Company Burgers": Burger_count,
+    "Frenchie Fries": Fries_count,
+    "Nuggets of Chicken": Nugget_count,
+    "Coke Ka Cola": Coke_count,
+    "Soft C Softy": Softy_count,
+    "Veggie Veg Burger": VegBurg_count
+}
+
+prices = {
+    "Company Burgers": 120,
+    "Frenchie Fries": 100,
+    "Nuggets of Chicken": 180,
+    "Coke Ka Cola": 100,
+    "Soft C Softy": 80,
+    "Veggie Veg Burger": 90
+}
+
+items = []
+
+for item_name,count in menu_items.items():
+    if count>=1:
+        items.extend([item_name] * count)
+
+
+st.write(f"🛒 Cart : ({len(items)} items :arrow_down: )",items)
+
+
+st.markdown(" ##  :memo: Order Summary")
+amt = 0
+for item_name,count in menu_items.items():
+    if count>=1:
+        st.write(f"{count}x {item_name}")
+        amt = amt + prices[item_name]*count
+
+
+st.write(f"Total Amount: {amt} ")
 
 st.button("Order Now")
